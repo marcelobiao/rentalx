cd6b11de583a1bfeb149071a40db40fddf50b5c2
marcelobiao <biao.marcelo@gmail.com>
2021-07-09 18:09:19 -0300

    Refatorando dependências de category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,19 +1,24 @@
-import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
     name: string,
     description: string
 }
 
+@injectable()
 class CreateCategoryUseCase {
 
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+    constructor(
+        @inject('CategoriesRepository')
+        private categoriesRepository: ICategoriesRepository
+    ) {}
 
     async execute({name, description}: IRequest): Promise<void>{
         const categoryAlreadExists = await this.categoriesRepository.findByName(name)
 
         if(categoryAlreadExists) {
-            throw new Error("Category already exists!")
+            throw new Error('Category already exists!')
         }
 
         this.categoriesRepository.create({name, description})
